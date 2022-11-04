81c1a26815309552ed34bcaa9d54856782d2e337
allantomas <32474234+RichardTomas@users.noreply.github.com>
2022-11-03 20:07:56 -0700

    controller update

--- MonthConverter/src/main/java/com/example/controller/MonthController.java
+++ MonthConverter/src/main/java/com/example/controller/MonthController.java
@@ -1,11 +1,12 @@
 package com.example.controller;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class MonthController {
-    @RequestMapping(value="/month/{monthNumber}", method=RequestMethod.GET)
+    @RequestMapping(value = "/month/{monthNumber}", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public String getMonth(@PathVariable String monthNumber) {
 
@@ -34,6 +35,39 @@ public class MonthController {
                 return "November";
             case "12":
                 return "December";
+            default:
+                throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Invalid Month Number Specified");
+        }
+    }
+
+    @RequestMapping(value = "/monthAbbreviation/{monthNumber}", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public String getMonthAbbreviation(@PathVariable String monthNumber) {
+        switch (monthNumber) {
+            case "1":
+                return "Jan";
+            case "2":
+                return "Feb";
+            case "3":
+                return "Mar";
+            case "4":
+                return "Apr";
+            case "5":
+                return "May";
+            case "6":
+                return "Jun";
+            case "7":
+                return "Jul";
+            case "8":
+                return "Aug";
+            case "9":
+                return "Sep";
+            case "10":
+                return "Oct";
+            case "11":
+                return "Nov";
+            case "12":
+                return "Dec";
             default:
                 throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Invalid Month Number Specified");
         }
